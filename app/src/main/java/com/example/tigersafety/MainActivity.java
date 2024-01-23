c4241880128873ea438787019b5a0c14e60f9611
nomakhosi Mahlangu <nomakhosimahlangu05@gmail.com>
2024-01-23 09:30:57 +0200

    attempted google signin and more firebase stuff

--- app/src/main/java/com/example/tigersafety/MainActivity.java
+++ app/src/main/java/com/example/tigersafety/MainActivity.java
@@ -1,23 +1,50 @@
 package com.example.tigersafety;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
     private Button login;
     private Button signup;
+    private ImageButton googleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        googleBtn = findViewById(R.id.googleBtn);
         login = findViewById(R.id.login);
         signup = findViewById(R.id.signup);
+
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = gsc.getSignInIntent();
+                startActivityForResult(signInIntent,1000);
+            }
+        });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,4 +64,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void  onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                finish();
+                Intent intent = new Intent(MainActivity.this, Screen_1.class);
+                startActivity(intent);
+
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/tigersafety/Screen_3.java
+++ app/src/main/java/com/example/tigersafety/Screen_3.java
@@ -5,9 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class Screen_3 extends AppCompatActivity {
 
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
     private Button button4;
     private EditText editText;
 
@@ -15,5 +23,16 @@ public class Screen_3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen3);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        button4 = findViewById(R.id.button4);
+        editText = findViewById(R.id.editTextText);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null){
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+        }
     }
 }
