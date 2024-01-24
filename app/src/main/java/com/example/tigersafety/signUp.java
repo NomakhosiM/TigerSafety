b17e7177d016b2e95ceb395123141a8d5cbc129b
Nomakhosi Mahlangu <nomakhosimahlangu05@gmail.com>
2024-01-24 18:49:45 +0200

    added firebase and firebase auth-signup

--- app/src/main/java/com/example/tigersafety/signUp.java
+++ app/src/main/java/com/example/tigersafety/signUp.java
@@ -1,14 +1,23 @@
 package com.example.tigersafety;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signUp extends AppCompatActivity {
 
@@ -17,10 +26,14 @@ public class signUp extends AppCompatActivity {
     private EditText pwcSignup;
     private Button gotoprofile;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
 
         emailSignup = findViewById(R.id.emailSignup);
         pwSignup = findViewById(R.id.pwSignup);
@@ -57,12 +70,33 @@ public class signUp extends AppCompatActivity {
 
                 else {
                     //send password and email to firebase
-                    Intent intent = new Intent(signUp.this, setProfile.class);
-                    startActivity(intent);
+                    mAuth.createUserWithEmailAndPassword(EmailSignUp,PWSignup)
+                            .addOnCompleteListener(signUp.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Intent intent = new Intent(signUp.this, setProfile.class);
+                                        startActivity(intent);
+                                    }else{
+                                        Log.w("user exeception", "Create user unsuccessful", task.getException());
+                                        Toast.makeText(signUp.this,"please try again", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
                 }
 
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null){
+            currentUser.reload();
+        }
     }
 }
 
